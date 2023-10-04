474127090c1cc117fc21393c859c1bcaf75e4e88
marin <marin@studiolab.ai>
2023-10-05 00:10:39 +0900

    docs : Add ignore .DS_Store

--- chapter_0/0_2.py
+++ chapter_0/0_2.py
@@ -19,7 +19,7 @@ print(" ================================================= ")
 import requests
 
 params = dict(q='Sausages',format='json')
-parsed = requests.get ('http://api.duckduckgo.com/', params=params).json()
+parsed = requests.get('http://api.duckduckgo.com/', params=params).json()
 results = parsed['RelatedTopics']
                  
 for r in results:
