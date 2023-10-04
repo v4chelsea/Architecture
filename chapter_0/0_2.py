03341c6b4a4c724f56ae3bf1b9d435aaf4bd7a6c
marin <marin@studiolab.ai>
2023-10-05 00:09:46 +0900

    feat : Study Chapter 0

---
+++ chapter_0/0_2.py
@@ -0,0 +1,27 @@
+import json
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
+params = dict(q="Sausages", format="json")
+handle = urlopen('http://api.duckduckgo.com' + '?' + urlencode(params))
+raw_text = handle.read().decode('utf8')
+parsed = json.loads(raw_text)
+
+resutls = parsed['RelatedTopics']
+
+for r in resutls:
+    if "Text" in r:
+        print(r['FirstURL'] + " - " + r['Text'])
+
+print(" ================================================= ")
+
+
+import requests
+
+params = dict(q='Sausages',format='json')
+parsed = requests.get ('http://api.duckduckgo.com/', params=params).json()
+results = parsed['RelatedTopics']
+                 
+for r in results:
+    if 'Text' in r:
+        print(r['FirstURL'] + " - " + r['Text'])
